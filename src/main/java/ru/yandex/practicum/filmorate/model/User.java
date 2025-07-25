882566748dcf4e5b68d83ee81bfa8efd5113dabf
Карина <karinkademchenko@yandex.ru>
2025-07-25 18:22:24 +0300

    Add User

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+import lombok.NonNull;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+    @NonNull
+    private int id;
+
+    @NonNull
+    private String email;
+
+    @NonNull
+    private String login;
+
+    private String name;
+
+    @NonNull
+    private LocalDate birthday;
+}
