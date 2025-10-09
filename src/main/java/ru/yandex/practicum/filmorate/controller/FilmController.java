208102c4eac4e1d46aa2ba672ce87b184a7f64e6
Карина <karinkademchenko@yandex.ru>
2025-10-09 22:05:29 +0300

    Обновлены контроллеры

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,20 +1,26 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
+
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.dto.FilmDto;
+import ru.yandex.practicum.filmorate.dto.NewFilmRequest;
+import ru.yandex.practicum.filmorate.dto.UpdateFilmRequest;
 import ru.yandex.practicum.filmorate.exceptions.ErrorHandler;
 
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.film.FilmService;
 
 import java.util.Collection;
 
 @Slf4j
 @RestController
+@Validated
 @RequestMapping("/films")
 @RequiredArgsConstructor
 @Import(ErrorHandler.class)
@@ -23,52 +29,53 @@ public class FilmController {
     private final FilmService filmService;
 
     @GetMapping
-    public Collection<Film> findAll() {
+    public Collection<FilmDto> findAllFilms() {
         log.info("Запрос на получение списка всех фильмов.");
         return filmService.getAllFilms();
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
-    public Film getFilmById(@PathVariable int id) {
+    public FilmDto getFilmById(@PathVariable("id") @Positive Long id) {
         log.info("Запрос на получение фильма с id={}.", id);
         return filmService.getFilmById(id);
     }
 
     @GetMapping("/popular")
-    public Collection<Film> getPopularFilms(@RequestParam(defaultValue = "10") int count) {
+    public Collection<FilmDto> getPopularFilms(@RequestParam(defaultValue = "10") int count) {
         log.info("Запрос на получение {} популярных фильмов.", count);
         return filmService.getPopularFilms(count);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Film create(@Valid @RequestBody Film film) {
-        log.info("Запрос на добавление нового фильма {}.", film);
-        return filmService.create(film);
+    public FilmDto createFilm(@Valid @RequestBody NewFilmRequest request) {
+        log.info("Запрос на добавление нового фильма {}.", request);
+        return filmService.createFilm(request);
     }
 
     @PutMapping
-    public Film update(@Valid @RequestBody Film updatedFilm) {
-        log.info("Запрос на обновление данных фильма {}.", updatedFilm);
-        return filmService.update(updatedFilm);
+    @ResponseStatus(HttpStatus.OK)
+    public FilmDto updateFilm(@Valid @RequestBody UpdateFilmRequest request) {
+        log.info("Запрос на обновление данных фильма {}.", request);
+        return filmService.updateFilm(request);
     }
 
     @PutMapping("/{id}/like/{userId}")
-    public void addLike(@PathVariable int id, @PathVariable long userId) {
+    public void addLike(@PathVariable("id") @Positive Long id, @PathVariable("userId") @Positive Long userId) {
         log.info("Запрос на добавление лайка фильму с id={} от пользователя с id={}.", id, userId);
         filmService.addLike(id, userId);
     }
 
-    @DeleteMapping("/{id}")
-    public void delete(@Valid @RequestBody Film film) {
-        log.info("Запрос на удаление фильма {}.", film);
-        filmService.delete(film);
-    }
-
     @DeleteMapping("/{id}/like/{userId}")
-    public void deleteLike(@PathVariable int id, @PathVariable long userId) {
+    public void deleteLike(@PathVariable("id") @Positive Long id, @PathVariable("userId") @Positive Long userId) {
         log.info("Запрос на удаление лайка к фильму с id={} от пользователя с id={}.", id, userId);
         filmService.deleteLike(id, userId);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteFilm(@PathVariable("id") @Positive Long id) {
+        log.info("Запрос на удаление фильма с id {}.", id);
+        filmService.deleteFilm(id);
     }
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -6,10 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.dto.NewUserRequest;
+import ru.yandex.practicum.filmorate.dto.UpdateUserRequest;
 import ru.yandex.practicum.filmorate.exceptions.ErrorHandler;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.user.UserService;
 
+import ru.yandex.practicum.filmorate.dto.UserDto;
 import java.util.Collection;
 
 @Slf4j
@@ -18,54 +20,53 @@ import java.util.Collection;
 @RequiredArgsConstructor
 @Import(ErrorHandler.class)
 public class UserController {
-
     private final UserService userService;
 
     @GetMapping
-    public Collection<User> findAll() {
+    public Collection<UserDto> findAllUsers() {
         log.info("Запрос на получение списка всех пользователей.");
         return userService.getAllUsers();
     }
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable long id) {
+    public UserDto getUserById(@PathVariable Long id) {
         log.info("Запрос на получение пользователя с id={}.", id);
         return userService.getUserById(id);
     }
 
     @GetMapping("/{id}/friends")
-    public Collection<User> getFriends(@PathVariable long id) {
+    public Collection<UserDto> getFriends(@PathVariable Long id) {
         log.info("Запрос на получение друзей пользователя с id={}.", id);
         return userService.getAllFriends(id);
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
-    public Collection<User> getCommonFriends(@PathVariable long id, @PathVariable long otherId) {
+    public Collection<UserDto> getCommonFriends(@PathVariable Long id, @PathVariable Long otherId) {
         log.info("Запрос на получение общих друзей пользователей с id={} и id={}.", id, otherId);
         return userService.getCommonFriends(id, otherId);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public User create(@Valid @RequestBody User user) {
-        log.info("Запрос на добавление нового пользователя {}.", user);
-        return userService.create(user);
+    public UserDto createUser(@Valid @RequestBody NewUserRequest request) {
+        log.info("Запрос на добавление нового пользователя {}.", request);
+        return userService.createUser(request);
     }
 
     @PutMapping
-    public User update(@Valid @RequestBody User updatedUser) {
-        log.info("Запрос на обновление данных пользователя {}.", updatedUser);
-        return userService.update(updatedUser);
+    public UserDto updateUser(@Valid @RequestBody UpdateUserRequest request) {
+        log.info("Запрос на обновление данных пользователя {}.", request);
+        return userService.updateUser(request);
     }
 
     @PutMapping("/{id}/friends/{newFriendId}")
-    public void addFriend(@PathVariable long id, @PathVariable long newFriendId) {
+    public void addFriend(@PathVariable Long id, @PathVariable Long newFriendId) {
         log.info("Запрос пользователя с id={} на добавление в друзья пользователя с id={}.", id, newFriendId);
         userService.addFriend(id, newFriendId);
     }
 
     @DeleteMapping("/{id}/friends/{friendId}")
-    public void deleteFriend(@PathVariable long id, @PathVariable long friendId) {
+    public void deleteFriend(@PathVariable Long id, @PathVariable Long friendId) {
         log.info("Запрос пользователя с id={} на удаление из друзей пользователя с id={}.", id, friendId);
         userService.deleteFriend(id, friendId);
     }
